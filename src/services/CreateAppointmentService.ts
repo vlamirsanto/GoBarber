d264abd99f8efd48c4b38db724fb08db0b32ab39
Vlamir Santo <vlamir.santo@dotlib.com.br>
2021-03-20 10:10:49 -0300

    Utilização do Postgres.

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,26 +9,24 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository;
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ date, provider }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmenInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmenInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
     if (findAppointmenInSameDate)
       throw Error('This appointment is already booked');
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
