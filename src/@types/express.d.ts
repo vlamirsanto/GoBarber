cc7bb249182786f1b1284909becf6e9699c5cd0d
Vlamir Santo <vlamir.santo@dotlib.com.br>
2021-03-25 20:58:07 -0300

    Upload de imagens.

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Necessário para declarar uma tipagem dentro de uma biblioteca importada e utilizada
+ */
 declare namespace Express {
   export interface Request {
     user: {
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -28,5 +34,34 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
