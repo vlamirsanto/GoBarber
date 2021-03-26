d7f840d82aec93f6626636f58ff503e74ddc3801
Vlamir Santo <vlamir.santo@dotlib.com.br>
2021-03-25 21:52:29 -0300

    Tratando exceções.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,32 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+// Tratativa GLOBAL de errors
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
+);
 
 app.listen(3333, () => console.log('Server started on post 3333!'));
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -22,11 +23,12 @@ class AuthenticateUserService {
       where: { email },
     });
 
-    if (!user) throw Error('Incorrect email/password combination.');
+    if (!user) throw new AppError('Incorrect email/password combination.', 401);
 
     const passwordMatched = await compare(password, user.password);
 
-    if (!passwordMatched) throw Error('Incorrect email/password combination.');
+    if (!passwordMatched)
+      throw new AppError('Incorrect email/password combination.', 401);
 
     const { secret, expiresIn } = authConfig.jwt;
 
