c20c4f8f583bff5361df670c33706c7fe9d1f9d4
Vlamir Santo <vlamir.santo@dotlib.com.br>
2021-04-21 10:51:43 -0300

    Configurando e testando a criação de Appointments.

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
+
+  it('should not ble able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2021, 3, 21, 10, 43);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
